61264fed4fdec9f66c8760293c900cab6e55ecdd
Boris Muzalevskii <bmuza57@gmail.com>
2021-07-03 19:00:51 +0300

    Add hero image and down button events

--- src/index.js
+++ src/index.js
@@ -1,3 +1,40 @@
 import './index.scss';
+import Face from './assets/assets/Male-4-Walk.png';
 
-console.log('####', 'Hello');
+const canvas = document.getElementById('game');
+const ctx = canvas.getContext('2d');
+const spriteW = 48;
+const spriteH = 48;
+const shots = 3;
+let cycle = 0;
+let bottomPress = false;
+let pY = 0;
+
+const img = document.createElement('img');
+img.src = Face;
+
+const keyDownHandler = (e) => {
+  if (e.key === 'Down' || e.key === 'ArrowDown') {
+    bottomPress = true;
+  }
+};
+
+const keyUpHandler = (e) => {
+  if (e.key === 'Down' || e.key === 'ArrowDown') {
+    bottomPress = false;
+  }
+};
+
+document.addEventListener('keydown', keyDownHandler);
+document.addEventListener('keyup', keyUpHandler);
+
+img.addEventListener('load', () => {
+  setInterval(() => {
+    if (bottomPress) {
+      pY += 10;
+      cycle = (cycle + 1) % shots;
+    }
+    ctx.clearRect(0, 0, 600, 600);
+    ctx.drawImage(img, cycle * spriteW, 0, spriteH, spriteW, 0, pY, 48, 48);
+  }, 120);
+});
